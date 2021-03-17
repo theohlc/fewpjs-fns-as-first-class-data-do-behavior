01b5c21c720f1dec5d050fc7a2b2035f0618d326
theo <teddyhoover@gmail.com>
2021-03-17 13:37:44 -0500

    commit tree 974cf5c013359983ed324daf2fdaf77be3e5569b

--- index.js
+++ index.js
@@ -10,4 +10,22 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time) {
+  let t = time.split(':');
+  let hrs = parseInt(t[0]);
+  if (hrs < 12) {
+    return "Good Morning";
+  } else if (hrs >= 12 && hrs < 17) {
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+  }
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(str) {
+  let e = document.getElementById('greeting')
+  e.innerText = str
+}
